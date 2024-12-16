53d2b496cfb0622ebf46bec25e3d772b0d0beea3
unknown <h33r.kh4n@gmail.com>
2024-12-16 23:10:51 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-
+import React from "react";
 import Categories from "./components/Categories";
 import CompanyLogo from "./components/companyLogo";
 
 import Hero from "./components/Hero";
 import HotProduct from "./components/hotProducts";
+import Navbar from "./components/Navbar";
 import OurProduct from "./components/ourProduct";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Navbar />
       <CompanyLogo />
       <Categories />
       <HotProduct />
